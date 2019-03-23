89437faeab7575d2da0b3a2689d60f80a5eec498
Daniel Attiach <danielattiach@gmail.com>
2019-03-23 20:38:21 +0200

    added barak's guide

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('barak/', views.barak, name='barak')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
   return render(request, 'pages/index.html')
+
+def barak(request):
+  return render(request, 'pages/barak.html')
